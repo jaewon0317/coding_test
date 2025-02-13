216c82965aa923b5429784f61b11f145033ae7bf
jaewon0317 <cloudlala0@gmail.com>
2025-02-13 11:30:59 +0900

    Update JadenCase 문자열 만들기.py

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,13 +1,13 @@
 def solution(s):
-    s_splitted = s.split(' ')
+    s_splitted = s.split(' ')# 공백 기준으로 쪼갬
     ans = ''
     for i in range(len(s_splitted)):
-        if s_splitted[i] == '':
+        if s_splitted[i] == '': #공백이 연속될때의 처리
             ans += ' '
-        elif i == len(s_splitted) - 1:
+        elif i == len(s_splitted) - 1: # 마지막 문자열 처리
             ans += s_splitted[i].capitalize()
         else:
-            ans += s_splitted[i].capitalize() + ' '
+            ans += s_splitted[i].capitalize() + ' ' #문자열 처리
     if s_splitted[-1] == '':
         ans = ans[:-1]
     return ans
