da5a8b86ea78c37627afca6230c7888968085fa0
jaewon0317 <cloudlala0@gmail.com>
2025-07-02 19:50:03 +0900

    [level 1] Title: 실패율, Time: 1644.12 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -3,18 +3,16 @@
 # 3. key 기준으로 내림차순 정렬
 def solution(N, stages):
     error_rate={}
+    stage_list = []
+    people = len(stages)
     for i in range(1,N+1):
-        i_num=0
-        i_bigger_num=0
-        for j in stages:
-            if j == i:
-                i_num +=1
-            elif j > i:
-                i_bigger_num +=1
-        if i_bigger_num+i_num == 0:
-            error_rate[i]= 0
-        else:
-            error_rate[i]=i_num/(i_bigger_num+i_num)
-    print(error_rate)
+        count = stages.count(i)
+        stage_list.append(count)
+    for i in range(N):
+        if people == 0:
+            error_rate[i+1] = 0
+        else :
+            error_rate[i+1] = stage_list[i]/people
+            people -= stage_list[i]
     sorted_keys_get = sorted(error_rate, key=error_rate.get, reverse=True)
     return sorted_keys_get
