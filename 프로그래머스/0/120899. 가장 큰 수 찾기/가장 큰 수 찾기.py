3b5a13c09dce64ad17aad99228e4e4e4d2dfc8e1
jaewon0317 <cloudlala0@gmail.com>
2025-07-02 14:56:39 +0900

    Update 가장 큰 수 찾기.py
    효율성 개선

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,5 +1,10 @@
+# def solution(array):
+#     for index,value in enumerate(array):
+#         if value == max(array):
+#             result = [value,index]
+#             return result
+
 def solution(array):
-    for index,value in enumerate(array):
-        if value == max(array):
-            result = [value,index]
-            return result
+    max_value = max(array)
+    index = array.index(max_value)
+    return [max_value, index]
